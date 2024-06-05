c98589218b2c3a77e860ceaba7ca55924a08c863
Chrystian Carlos <Chrystiancarlosdasilva@gmail.com>
2024-06-04 23:19:45 -0300

    "build: adicionando nova função que esconde a nav bar ao rolar a página para baixo"

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const itemsNav = document.getElementById("itemsNav")
 const menu = document.getElementById("burguer")
+const nav = document.querySelector(".nav")
+
 
 function clickMenu() {
 
@@ -14,6 +16,8 @@ menu.textContent = "close"
 
 }
 
+
+
 function menuR(){
     if(window.innerWidth < 785) {
         burguer.style.display = "block"
@@ -23,6 +27,25 @@ function menuR(){
         itemsNav.classList.remove("hidden")
     }
 }
+
+var lastScrollTop = 0;
+
+window.addEventListener("scroll", function() {
+    var currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+    
+    if (currentScroll > lastScrollTop) {
+        // Scroll está indo para baixo
+        console.log("Scroll para baixo");
+        nav.classList.add("hidden")
+        
+    } else {
+        // Scroll está indo para cima ou parado
+        console.log("Scroll para cima ou parado");
+        nav.classList.remove("hidden")
+    }
+    
+    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // Para dispositivos móveis
+}, false);
 
 menuR()
 setInterval (menuR, 10)
